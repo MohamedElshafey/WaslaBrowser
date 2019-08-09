c5a8f24221bee3f32c7c3ad42c2e23114e28da59
Mohamed Elshafey <mohamedes335@gmail.com>
2019-08-09 15:53:32 +0200

    Unit test to check url validation

---
+++ app/src/test/java/com/wasla/browser/UrlTest.kt
@@ -0,0 +1,22 @@
+package com.wasla.browser
+
+import com.google.common.truth.Truth.assertThat
+import com.wasla.browser.util.isValidUrl
+import org.junit.Test
+
+class UrlTest {
+
+    @Test
+    fun urlValidationTest() {
+        assertThat(isValidUrl("test@email.com")).isFalse()
+        assertThat(isValidUrl("fb.com")).isTrue()
+        assertThat(isValidUrl("www.facebook.com")).isTrue()
+        assertThat(isValidUrl("www.youtube.com")).isTrue()
+        assertThat(isValidUrl("http://wuzzuf.net")).isTrue()
+        assertThat(isValidUrl("eg.linkedin.com")).isTrue()
+        assertThat(isValidUrl("eg.linkedin")).isFalse()
+        assertThat(isValidUrl("https://truth.dev")).isTrue()
+        assertThat(isValidUrl("truth.dev")).isTrue()
+    }
+
+}
